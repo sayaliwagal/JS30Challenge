755a3b6f5ba42b8f9c92fc7fd5728d9726096125
Sayali Wagal <sayaliwagal90@gmail.com>
2025-05-10 03:26:22 +0530

    "Successfully completed Day2 of JS-30-challenge"

---
+++ CSS+JS Clock/script.js
@@ -0,0 +1,23 @@
+const secondHand = document.querySelector('.sec-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate(){
+    const now = new Date();
+//    calculate the sec on the  current date 
+    const sec = now.getSeconds();
+    // convert the calculated seconds to degeree to rated the  sec hand of clock 
+    const secDegress = ((sec /60 ) * 360) + 90;
+    //    calculate the mintues on the  current date 
+    const min = now.getMinutes();
+        // convert the calculated mintues to degeree to rated the  mintues hand of clock
+    const minDegress = ((min/60) * 360) + 90;
+    //    calculate the hourse on the  current date 
+       const hours = now.getHours();
+        // convert the calculated hours to degeree to rated the  hours hand of clock
+       const hoursDegrees = ((hours/ 24) * 360) + 90;
+   secondHand.style.transform = `rotate(${secDegress}deg)`;
+   minHand.style.transform =`rotate(${minDegress}deg)`;
+   hourHand.style.transform = `rotate(${hoursDegrees}.deg);`
+}
+setInterval(setDate, 1000);
